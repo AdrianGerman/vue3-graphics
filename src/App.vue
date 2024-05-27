48c5d23bcadbdb68787e7e89fbe7eb7e4b8eef25
AdrianGerman <adrian.german1019@gmail.com>
2024-05-27 11:17:52 -0600

    remove unnecessary things

--- src/App.vue
+++ src/App.vue
@@ -3,16 +3,7 @@ import OperationsDashboard from './components/OperationsDashboard.vue'
 </script>
 
 <template>
-  <main>
-    <OperationsDashboard />
-  </main>
+  <OperationsDashboard />
 </template>
 
-<style scoped>
-main {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-}
-</style>
+<style></style>
--- src/components/OperationsDashboard.vue
+++ src/components/OperationsDashboard.vue
@@ -1,9 +1,17 @@
 <template>
     <main>
-        <section>
+
+        <section class="graphic-1">
+            <h1>titulo principal de la paella</h1>
+            <b>el perro que baila</b>
+            <p class="elpe">
+                <span>sitos: 39</span>
+                <span>enlaces: 23</span>
+            </p>
             <Bar id="my-chart-id" :options="chartOptions" :data="chartData" />
         </section>
-        <section>
+
+        <section class="graphic-2">
             <div>
                 <h5>Hola macarena</h5>
                 <h3>239458932</h3>
@@ -19,6 +27,7 @@
                 <Doughnut id="my-doughnut-chart-2" :options="options" :data="data" />
             </div>
         </section>
+
     </main>
 </template>
 
@@ -53,7 +62,7 @@ const options = {
 </script>
 
 <style scoped>
-/* Asegúrate de que los contenedores tienen tamaño */
+/* Estilos de las graficas ignora o eres musulman */
 section {
     display: flex;
     justify-content: space-around;
@@ -65,10 +74,40 @@ section>div {
 }
 
 /* lo mio papu */
-main {
-    background-color: antiquewhite;
-    width: 612pt;
-    height: 750pt;
 
+main {
+    display: grid;
+    grid-template-rows: 1fr 1fr;
+    gap: 5%;
+    background-color: #c7c7c7;
+    width: 816px;
+    height: 1000px;
+    padding: 20px;
+    box-sizing: border-box;
+}
+
+.graphic-1 {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    gap: 1rem;
+}
+
+.elpe span {
+    margin: 6px;
+}
+
+.graphic-2 {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+}
+
+.graphic-2 div {
+    display: flex;
+    flex-direction: column;
+    justify-self: center;
+    align-items: center;
 }
 </style>
